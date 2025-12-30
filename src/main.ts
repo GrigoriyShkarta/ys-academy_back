df3915b6ec484d270871a79860ecfb24fde006ae
hryhorii_shkarta <grishkoshkarta1@gmail.com>
2025-12-30 15:44:30 +0200

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://ys-academy.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://ys-academy.vercel.app',
+      'https://ys-academy-dev.vercel.app/',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
