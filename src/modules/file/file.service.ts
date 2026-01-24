5b29b5d67369e6d75dfdce8ec10f142523ef2874
hryhorii_shkarta <grishkoshkarta1@gmail.com>
2026-01-24 14:53:43 +0200

    test var

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -1,16 +1,18 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
 import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 type FileType = 'image' | 'video' | 'raw';
 
 @Injectable()
-export class FileService {
+export class FileService implements OnModuleInit {
+  onModuleInit() {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+      api_key: process.env.CLOUDINARY_API_KEY!,
+      api_secret: process.env.CLOUDINARY_API_SECRET!,
+    });
+  }
+
   async uploadFile(
     file: Express.Multer.File,
     type: FileType,
@@ -24,15 +26,17 @@ export class FileService {
         },
         (error, result) => {
           if (error) return reject(new BadRequestException(error.message));
-          if (!result)
+          if (!result) {
             return reject(
               new BadRequestException(
                 'Cloudinary upload returned empty result',
               ),
             );
+          }
           resolve(result);
         },
       );
+
       upload.end(file.buffer);
     });
   }
