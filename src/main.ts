4727e41648a457286fa1f1df05d4e7d3f719865f
hryhorii_shkarta <grishkoshkarta1@gmail.com>
2026-03-09 13:11:08 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     // ],
     origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
